b0d3bc9178bfcd2d6189b271ee7f23cf1283c095
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-02-19 17:31:40 -0300

    Ajuste do feedback e responsividade para celular

--- HUTTER/script.js
+++ HUTTER/script.js
@@ -1,67 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuHamburger = document.querySelector('.menu-hamburger');
-    const nav = document.getElementsByClassName('menu_navegacao')[0];
-    const closeButton = document.querySelector('.menu_navegacao .close-button');
-
-    // Função para alternar a visibilidade do menu de navegação
-    function toggleNav() {
-        nav.style.display = nav.style.display === "block" ? "none" : "block";
-    }
-
-    // Adiciona event listener ao menu hambúrguer
-    menuHamburger.addEventListener('click', toggleNav);
-    closeButton.addEventListener('click', toggleNav);
-
-    // Adiciona event listeners para cada item do menu
-    document.querySelectorAll('.menu_navegacao .menu li a').forEach(function(item) {
-        item.addEventListener('click', function() {
-            // Esconde o menu de navegação em dispositivos móveis
-            if (window.matchMedia('(max-width: 667px)').matches) {
-                nav.style.display = 'none';
-            }
-        });
-    });
-
-    // Função para lidar com a mudança de tamanho da janela
-    function handleWindowResize() {
-        // Verifica se estamos em um dispositivo móvel (menos que 668px)
-        if (window.matchMedia('(max-width: 820px)').matches) {
-            menuHamburger.style.display = 'block';
-            nav.style.display = 'none'; // Esconde o menu de navegação
-        }
-        // Para tablets e desktops (maior que 820px)
-        else {
-            menuHamburger.style.display = 'none';
-            nav.style.display = 'block'; // Mostra o menu de navegação
-        }
-    }
-
-    // Adiciona o event listener ao redimensionamento da janela
-    window.addEventListener('resize', handleWindowResize);
-
-    // Chama a função imediatamente para definir o estado inicial
-    handleWindowResize();
-});
-
-
-
-//Ação criada para fazer o scroll das inhas imagens da sessão sobre a empresa.
-
-document.addEventListener("DOMContentLoaded", function() {
-    const imageContainer = document.querySelector(".container.lista_imagens");
-    const imageWidth = imageContainer.querySelector(".imagens_servicos").offsetWidth;
-    const gapWidth = 18; // O valor do gap definido no CSS
-
-    document.querySelector(".prev").addEventListener("click", function() {
-        // Move o scroll para a esquerda
-        imageContainer.scrollBy({ left: -(imageWidth + gapWidth), behavior: 'smooth' });
-    });
-
-    document.querySelector(".next").addEventListener("click", function() {
-        // Move o scroll para a direita
-        imageContainer.scrollBy({ left: imageWidth + gapWidth, behavior: 'smooth' });
-    });
-});
 
 
 // Ajuste para os feedbacks
@@ -93,8 +29,8 @@ allStar.forEach((item, idx)=> {
 // Para manipulação do botão submit do formulário de feedback
 
 document.addEventListener('DOMContentLoaded', function() {
-    const submitBtn = document.getElementById('submitBtn');
-    const feedbackForm = document.getElementById('feedbackForm');
+    const feedbackForm = document.getElementById('feedbackFormId');
+    
 
     feedbackForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevenir o comportamento padrão de submissão do formulário
@@ -105,16 +41,12 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Rating:', ratingValue.value);
         console.log('Opinion:', feedbackForm.opinion.value);
 
+        // Adicionar lógica para exibir um alerta quando a pesquisa é respondida
+        //alert('Obrigado por responder à pesquisa!');
+
+        // Agora você pode permitir a submissão do formulário
+        feedbackForm.submit();
+
     });
 });
 
-// Para manipulação do botão voltar para o menu
-
-document.addEventListener('DOMContentLoaded', function() {
-    const cancelButton = document.querySelector('.cancel');
-
-    cancelButton.addEventListener('click', function() {
-        // Aqui eu posso redirecionar o usuário para a página do menu ou executar outra ação desejada
-        window.location.href = 'index.html'; // Substitua 'url_do_seu_menu.html' pela URL do seu menu
-    })
-})
